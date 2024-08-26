bb46a956fd8bc93c9c3f1dabd96f5660fc505092
BirkofNiki <augastinendeti@gmail.com>
2024-08-26 21:29:59 +0300

    multiple pages created and routed, awaiting to add content

---
+++ src/components/navBar/navBarElements.js
@@ -0,0 +1,58 @@
+// Filename - ./components/Navbar.js
+
+import { FaBars } from "react-icons/fa";
+import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+	background: #1ccfa5;;
+	height: 85px;
+	display: flex;
+	justify-content: flex-end;
+	padding-right: 30px;
+	z-index: 12;
+    align-items: right;
+`;
+
+export const NavLink = styled(Link)`
+	color: white;
+    font-weight:700;
+    font-size:23px;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding: 0 1rem;
+	height: 100%;
+	cursor: pointer;
+	&.active {
+		color: #4d4dff;
+	}
+`;
+
+export const Bars = styled(FaBars)`
+	display: none;
+	color: #808080;
+	@media screen and (max-width: 768px) {
+		display: block;
+		position: absolute;
+		top: 0;
+		right: 0;
+		transform: translate(-100%, 75%);
+		font-size: 1.8rem;
+		cursor: pointer;
+	}
+`;
+
+export const NavMenu = styled.div`
+	display: flex;
+	align-items: center;
+	margin-right: -24px;
+	/* Second Nav */
+	/* margin-right: 24px; */
+	/* Third Nav */
+	/* width: 100vw;
+white-space: nowrap; */
+	@media screen and (max-width: 768px) {
+		display: none;
+	}
+`;
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+// Filename - pages/about.js
+
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>
+            AM-Vision-Tech is a Software Development and genereal Technology solutions partner.
+			</h1>
+		</div>
+	);
+};
+
+export default About;
