bf3467135909075cddb87e5800d9d2e2b9a5c599
BirkofNiki <augastinendeti@gmail.com>
2024-08-27 20:11:22 +0300

    added what we offer on the hero section/homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import React from "react";
 const Home = () => {
 	return (
 		<div>
-			<h1>Welcome to AM-Vision-Tech</h1>
+			<h1>Welcome to AM-Vision-Tech. Your solution to all Web Developement and Data Science problems.</h1>
 		</div>
 	);
 };
